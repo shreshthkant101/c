e7ecf01d03dea1cb69b6fe3919e8ff83158c50c3
shreshthkant101 <63898444+shreshthkant101@users.noreply.github.com>
2020-06-13 21:44:57 +0530

    Update game.js

--- js/game.js
+++ js/game.js
@@ -65,6 +65,10 @@ class Game{
             player.distance += 50;
             player.update();
         }
+        if(mousePressed && player.index != null){
+            player.distance += 50;
+            player.update();
+        }
         if(player.distance>3860){
             gameState = 2;
         }
